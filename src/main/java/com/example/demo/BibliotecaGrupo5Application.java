cf3ff12a8263c3854c9febeadc3cd868d4c77b27
MarcitaBustelo <marcitabuxtelo@gmail.com>
2024-11-20 12:21:27 +0100

    inicio

---
+++ src/main/java/com/example/demo/BibliotecaGrupo5Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaGrupo5Application {
+
+	public static void main(String[] args) {
+		
+		
+		SpringApplication.run(BibliotecaGrupo5Application.class, args);
+	}
+
+}
